82f547557d453f5b49360ca206f5af2a1f0d4c12
Qiming <cruiser31@gmail.com>
2013-10-09 03:16:26 +0000

    主页图片转换成flash轮换；360浏览器重叠故障排除

--- data/config.php
+++ data/config.php
@@ -30,6 +30,6 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2013-09-29 16:51:57');
+define('API_TIME', '2013-10-09 04:51:02');
 
 ?>
--- data/flashdata/dynfocus/data.js
+++ data/flashdata/dynfocus/data.js
@@ -1,7 +1,16 @@
-imgUrl1="data/afficheimg/20130929pdszrn.jpg";
+imgUrl1="data/afficheimg/20130929cvcjst.jpg";
 imgtext1="";
 imgLink1=escape("http://");
+imgUrl2="data/afficheimg/20131009rbjcke.jpg";
+imgtext2="";
+imgLink2=escape("http://");
+imgUrl3="data/afficheimg/20131009ddhfpf.jpg";
+imgtext3="";
+imgLink3=escape("http://");
+imgUrl4="data/afficheimg/20131009cizugc.jpg";
+imgtext4="";
+imgLink4=escape("http://");
 
-var pics=imgUrl1;
-var links=imgLink1;
-var texts=imgtext1;
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4;
